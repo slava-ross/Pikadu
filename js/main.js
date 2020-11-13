65985040c6fd0f42fcbb848950025edc7b107782
Yaroslav Yagodarov <slava-ross@yandex.ru>
2020-11-14 01:56:33 +0400

    work in progress

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ const emailInput = document.querySelector('.login-email');
 const passwordInput = document.querySelector('.login-password');
 const loginSignup = document.querySelector('.login-signup');
 
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+
 const listUsers = [
   {
     id: '01',
@@ -34,7 +37,12 @@ const listUsers = [
 const setUsers = {
   user: null,
   logIn(email, password) {
-  
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user)
+    } else {
+      alert('Пользователь не найден!');
+    }
   },
   logOut() {
     console.log('Выход');
@@ -42,29 +50,47 @@ const setUsers = {
   signUp(email, password) {
     if (!this.getUser(email)) {
       listUsers.push({email, password, displayName: email});
+      console.log(listUsers);
     } else {
-      alert('Пользователь уже зарегистрирован!');
+      alert('Пользователь с таким email уже зарегистрирован!');
     }
   },
   getUser(email) {
-    return listUsers.find(item => item.email === email)
+    return listUsers.find(item => item.email === email);
+  },
+  authorizedUser(user) {
+    this.user = user;
   }
 };
 
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+  console.log('user: ', user); 
+
+  if (user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+  }
+}
+
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  
   const emailValue = emailInput.value;
   const passwordValue = passwordInput.value;
   
-  setUsers.logIn(emailValue, passwordValue);
+  setUsers.logIn(emailValue, passwordValue, toggleAuthDom());
 });
 
 loginSignup.addEventListener('click', (event) => {
   event.preventDefault();
-   
   const emailValue = emailInput.value;
   const passwordValue = passwordInput.value;
   
-  setUsers.signUp(emailValue, passwordValue);
+  setUsers.signUp(emailValue, passwordValue, toggleAuthDom());
 });
+
+toggleAuthDom();
