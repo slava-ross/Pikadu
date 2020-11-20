2b0a187214d93983944039e74ddfc17ce488ec2b
Yaroslav Yagodarov <slava-ross@yandex.ru>
2020-11-20 18:27:54 +0400

    work in progress

--- js/main.js
+++ js/main.js
@@ -2,13 +2,8 @@
 let menuToggle = document.querySelector('#menu-toggle');
 // Создаем переменную, в которую положим меню
 let menu = document.querySelector('.sidebar');
-// отслеживаем клик по кнопке меню и запускаем функцию 
-menuToggle.addEventListener('click', function (event) {
-  // отменяем стандартное поведение ссылки
-  event.preventDefault();
-  // вешаем класс на меню, когда кликнули по кнопке меню 
-  menu.classList.toggle('visible');
-})
+
+const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
 
 const loginElem = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
@@ -18,6 +13,16 @@ const loginSignup = document.querySelector('.login-signup');
 
 const userElem = document.querySelector('.user');
 const userNameElem = document.querySelector('.user-name');
+
+const exitElem = document.querySelector('.exit');
+const editElem = document.querySelector('.edit');
+const editContainer = document.querySelector('.edit-container');
+
+const editUsername = document.querySelector('.edit-username');
+const editPhotoURL = document.querySelector('.edit-photo');
+const userAvatarElem = document.querySelector('.user-avatar');
+
+const postsWrapper = document.querySelector('.posts');
 
 const listUsers = [
   {
@@ -37,6 +42,10 @@ const listUsers = [
 const setUsers = {
   user: null,
   logIn(email, password, handler) {
+    if (!regExpValidEmail.test(email)) {
+      alert('email не валиден!');
+      return;
+    }
     const user = this.getUser(email);
     if (user && user.password === password) {
       this.authorizedUser(user);
@@ -45,8 +54,9 @@ const setUsers = {
       alert('Пользователь не найден!');
     }
   },
-  logOut() {
-    console.log('Выход');
+  logOut(handler) {
+    this.user = null;
+    handler();
   },
   signUp(email, password, handler) {
     if (!email.trim() || !password.trim()) {
@@ -62,12 +72,44 @@ const setUsers = {
       alert('Пользователь с таким email уже зарегистрирован!');
     }
   },
+  editUser(userName, userPhoto, handler) {
+    if (userName) {
+      this.user.displayName = userName;
+    }
+    if (userPhoto) {
+      this.user.photo = userPhoto;
+    }
+    handler();
+  },
   getUser(email) {
     return listUsers.find(item => item.email === email);
   },
   authorizedUser(user) {
     this.user = user;
   }
+};
+
+const setPosts = {
+  allPost: [
+    {
+      title: 'Заголовок поста 1',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags: ['свежее','новое','горячее','моё','случайность'],
+      author: 'slava@mail.ru',
+      date: '11.11.2020, 20:54:00',
+      like: 45,
+      comments: 12,
+    },
+    {
+      title: 'Заголовок поста 2',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags: ['свежее','новое','горячее','моё','случайность'],
+      author: 'slava@mail.ru',
+      date: '11.11.2020, 20:54:00',
+      like: 45,
+      comments: 12,
+    }
+  ]
 };
 
 const toggleAuthDom = () => {
@@ -78,28 +120,66 @@ const toggleAuthDom = () => {
     loginElem.style.display = 'none';
     userElem.style.display = '';
     userNameElem.textContent = user.displayName;
+    userAvatarElem.src = user.photo ? user.photo : userAvatarElem.src; // ||
   } else {
     loginElem.style.display = '';
     userElem.style.display = 'none';
   }
 }
 
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const emailValue = emailInput.value;
-  const passwordValue = passwordInput.value;
-  
-  setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
-  loginForm.reset(); 
-});
+const showAllPosts = () => {
+  postsWrapper.innerHTML = 'Тут что-то есть';
+}
 
-loginSignup.addEventListener('click', (event) => {
-  event.preventDefault();
-  const emailValue = emailInput.value;
-  const passwordValue = passwordInput.value;
+const init = () => {
   
-  setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
-  loginForm.reset(); 
-});
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
+    
+    setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
+    loginForm.reset(); 
+  });
+  
+  loginSignup.addEventListener('click', (event) => {
+    event.preventDefault();
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
+    
+    setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
+    loginForm.reset(); 
+  });
+  
+  exitElem.addEventListener('click', (event) => {
+    event.preventDefault();
+    setUsers.logOut(toggleAuthDom);
+  });
+  
+  editElem.addEventListener('click', (event) => {
+    event.preventDefault();
+    editContainer.classList.toggle('visible');
+    editUsername.value = setUsers.user.displayName;
+  });
+  
+  editContainer.addEventListener('submit', (event) => {
+    event.preventDefault();
+    setUsers.editUser(editUsername.value, editPhotoURL.value, toggleAuthDom);
+    editContainer.classList.remove('visible');
+  });
 
-toggleAuthDom();
+  // отслеживаем клик по кнопке меню и запускаем функцию 
+  menuToggle.addEventListener('click', function (event) {
+    // отменяем стандартное поведение ссылки
+    event.preventDefault();
+    // вешаем класс на меню, когда кликнули по кнопке меню 
+    menu.classList.toggle('visible');
+  })
+
+  showAllPosts();
+  toggleAuthDom();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  init();
+})
